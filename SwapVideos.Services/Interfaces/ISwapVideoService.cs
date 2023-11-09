e70670ed8f059e284231c4522d62e39a6a8fb904
Luis <lfponton@gmail.com>
2023-11-09 20:58:51 +0100

    connect to video service

---
+++ SwapVideos.Services/Interfaces/ISwapVideoService.cs
@@ -0,0 +1,10 @@
+﻿using SwapVideos.API.Client.Generated;
+
+namespace SwapVideos.Services.Interfaces;
+
+public interface ISwapVideoService
+{
+    Task<Video> GetVideoAsync(Guid videoId);
+    Task<IList<Video>> GetVideoAsync(int? page, int size);
+    Task<Video> TagVideoAsIndexedAsync(Guid videoId, bool isIndexed);
+}
---
+++ SwapVideos.Services/SwapVideoService.cs
@@ -0,0 +1,35 @@
+﻿using SwapVideos.API.Client.Generated;
+using SwapVideos.Services.Interfaces;
+
+namespace SwapVideos.Services;
+
+public class SwapVideoService: ISwapVideoService
+{
+    private readonly IVideoClient _videoClient;
+
+    public SwapVideoService(IVideoClient videoClient)
+    {
+        _videoClient = videoClient;
+    }
+
+    public async Task<Video> GetVideoAsync(Guid videoId)
+    {
+        return await _videoClient.GetVideoAsync(videoId);
+    }
+    
+    public async Task<IList<Video>> GetVideoAsync(int? page, int size)
+    {
+        var paginatedVideosRequest = new PaginatedVideosRequest()
+        {
+            Page = page,
+            Size = size
+        };
+        var response = await _videoClient.GetAllVideosAsync(paginatedVideosRequest);
+        return response.Videos.ToList();
+    }
+
+    public async Task<Video> TagVideoAsIndexedAsync(Guid videoId, bool isIndexed)
+    {
+        return await _videoClient.TagVideoAsIndexedAsync(videoId, isIndexed);
+    }
+}
