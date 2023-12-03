8c1fef990d167b244b6cdde6d15b5391b6848c33
Luis <lfponton@gmail.com>
2023-12-03 13:31:50 +0100

    js recorder

--- Keywords.Frontend/wwwroot/js/audioPlayer.js
+++ Keywords.Frontend/wwwroot/js/audioPlayer.js
@@ -14,7 +14,46 @@ window.player = {
                 document.body.removeChild(player)
             }, true
         );
+    },
+    recording: function (dotnetRef, id) {
+        const startButton = document.getElementById(id + 'start');
+        const stopButton = document.getElementById(id + 'stop');
+        const playButton = document.getElementById(id + 'play');
+        let audioRecorder;
+        let audioChunks = [];
+        navigator.mediaDevices.getUserMedia({audio: true})
+            .then(stream => {
+                // Initialize the media recorder object
+                audioRecorder = new MediaRecorder(stream);
+
+                // dataavailable event is fired when the recording is stopped
+                audioRecorder.addEventListener('dataavailable', e => {
+                    audioChunks.push(e.data);
+                });
+
+                // start recording when the start button is clicked
+                startButton.addEventListener('click', () => {
+                    audioChunks = [];
+                    audioRecorder.start();
+                });
+
+                // stop recording when the stop button is clicked
+                stopButton.addEventListener('click', () => {
+                    audioRecorder.stop();
+                });
+
+                // play the recorded audio when the play button is clicked
+                playButton.addEventListener('click', () => {
+                    const blobObj = new Blob(audioChunks, {type: 'audio/wav'});
+                    const audioUrl = URL.createObjectURL(blobObj);
+                    const audio = new Audio(audioUrl);
+                    audio.play();
+                });
+            }).catch(err => {
+
+            // If the user denies permission to record audio, then display an error.
+            // invoke dotnetRef method
+            //console.log('Error: ' + err);
+        });
     }
 }
-
-    
