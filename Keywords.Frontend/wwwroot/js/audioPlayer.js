77d12b1b181d914e9cdad40374fd5af511fdf557
Claudiu <75113646+143473@users.noreply.github.com>
2023-12-07 16:34:32 +0100

    adjusted speech to text implemented

--- Keywords.Frontend/wwwroot/js/audioPlayer.js
+++ Keywords.Frontend/wwwroot/js/audioPlayer.js
@@ -16,53 +16,89 @@ window.player = {
         );
     },
     recording: async function (dotnetRef, id) {
-        const startButton = document.getElementById(String(id + 'start'));
-        const playButton = document.getElementById(String(id + 'play'));
         let audioRecorder;
-        let audioChunks = [];
-        
-        window.fileDataStream = async function (){
-            const blobObj = new Blob(audioChunks, {type: 'audio/wav'});
-            const x = await blobObj.arrayBuffer();
+        let blob;
+
+        window.fileDataStream = async function (){            
+            const x = await blob.arrayBuffer();
             return new Uint8Array(x);
         }
-        async function startRecording(){
-            audioChunks = [];
-            audioRecorder.start();
-            setTimeout(() =>{
-                audioRecorder.stop();
-            },3000);
-        }
         
-        // async function pushErrorToBlazor(err){
-        //     await dotnetRef.invokeMethodAsync();
-        // }
-        
+        navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
         navigator.mediaDevices.getUserMedia({audio: true})
             .then(stream => {
-                // Initialize the media recorder object
-                audioRecorder = new MediaRecorder(stream);
                 
-                startRecording();
+                audioRecorder = new RecordRTC(stream, {
+                    type: 'audio',
+                    mimeType: 'audio/wav',
+                    recorderType: StereoAudioRecorder,
+                    audioBitsPerSecond: 256000,
+                    desiredSampRate: 16000,
+                    numberOfAudioChannels: 1,
+                })
+
+                audioRecorder.startRecording();
                 
-                // dataavailable event is fired when the recording is stopped
-                audioRecorder.ondataavailable = async function (e) {
-                    audioChunks.push(e.data);
-                }
-                
-                audioRecorder.onstop = async function (e ){
-                    await dotnetRef.invokeMethodAsync('Receive', id);
-                }
-                /*
-                // play the recorded audio when the play button is clicked
-                playButton.addEventListener('click', () => {
-                    const blobObj = new Blob(audioChunks, {type: 'audio/webm'});
-                    const audioUrl = URL.createObjectURL(blobObj);
-                    const audio = new Audio(audioUrl);
-                    audio.play();
-                }); */
+                setTimeout(() =>{
+                    audioRecorder.stopRecording(async function(){
+                        blob = audioRecorder.getBlob();
+                        await dotnetRef.invokeMethodAsync('Receive', id);
+                    });
+                },3000);
+
             }).catch(err => {
-                // pushErrorToBlazor(err);
+            // pushErrorToBlazor(err);
         });
     }
+    // recording: async function (dotnetRef, id) {
+    //           
+    //     const startButton = document.getElementById(String(id + 'start'));
+    //     const playButton = document.getElementById(String(id + 'play'));
+    //     let audioRecorder;
+    //     let audioChunks = [];
+    //    
+    //     window.fileDataStream = async function (){
+    //         const blobObj = new Blob(audioChunks, {type: 'audio/wav'});
+    //         const x = await blobObj.arrayBuffer();
+    //         return new Uint8Array(x);
+    //     }
+    //     async function startRecording(){
+    //         audioChunks = [];
+    //         audioRecorder.start();
+    //         setTimeout(() =>{
+    //             audioRecorder.stop();
+    //         },3000);
+    //     }
+    //    
+    //     // async function pushErrorToBlazor(err){
+    //     //     await dotnetRef.invokeMethodAsync();
+    //     // }
+    //    
+    //     navigator.mediaDevices.getUserMedia({audio: true})
+    //         .then(stream => {
+    //             // Initialize the media recorder object
+    //             audioRecorder = new MediaRecorder(stream);
+    //            
+    //             startRecording();
+    //            
+    //             // dataavailable event is fired when the recording is stopped
+    //             audioRecorder.ondataavailable = async function (e) {
+    //                 audioChunks.push(e.data);
+    //             }
+    //            
+    //             audioRecorder.onstop = async function (e ){
+    //                 await dotnetRef.invokeMethodAsync('Receive', id);
+    //             }
+    //             /*
+    //             // play the recorded audio when the play button is clicked
+    //             playButton.addEventListener('click', () => {
+    //                 const blobObj = new Blob(audioChunks, {type: 'audio/webm'});
+    //                 const audioUrl = URL.createObjectURL(blobObj);
+    //                 const audio = new Audio(audioUrl);
+    //                 audio.play();
+    //             }); */
+    //         }).catch(err => {
+    //             // pushErrorToBlazor(err);
+    //     });
+    // }
 }
--- Keywords.Services/SpeechService.cs
+++ Keywords.Services/SpeechService.cs
@@ -1,19 +1,49 @@
-﻿using Keywords.API.Client.Generated;
+﻿using System.Net.Http.Headers;
+using Keywords.API.Client.Generated;
 using Keywords.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
 
 namespace Keywords.Services;
 
 public class SpeechService : ISpeechService
 {
     private readonly ISpeechClient _speechClient;
-    
-    public SpeechService(ISpeechClient speechClient)
+    private readonly IConfiguration _configuration;
+    public SpeechService(ISpeechClient speechClient, IConfiguration configuration)
     {
         _speechClient = speechClient;
+        _configuration = configuration;
     }
 
-    public async Task<PronunciationAssessmentResponseDTO> CreatePronunciationAssessmentAsync(FileParameter audioFile, string language, string referenceText)
+    public async Task<PronunciationAssessmentResponseDTO> CreatePronunciationAssessmentAsync(Stream audioFile, string language, string referenceText)
     {
-        return await _speechClient.CreatePronunciationAssessmentAsync(language, referenceText, audioFile);
+        byte[] bytes;
+        var binaryReader = new BinaryReader(audioFile);
+        
+        bytes = binaryReader.ReadBytes((int)audioFile.Length);
+        var audioContent = new ByteArrayContent(bytes);
+
+        var form = new MultipartFormDataContent();
+        form.Headers.ContentType.MediaType = "multipart/form-data";
+        audioContent.Headers.ContentType = MediaTypeHeaderValue.Parse("audio/wav");
+        
+        form.Add(audioContent, "body", "body.wav");
+        
+        var httpClient = new HttpClient();
+
+        var urlBuilder= new System.Text.StringBuilder();
+        var url = _configuration.GetConnectionString("keywordsbackend");
+        var url2 = urlBuilder.Append(url).Append("/speech").Append($"?language={language}")
+            .Append($"&referenceText={referenceText}").ToString();
+
+        var response = await httpClient.PostAsync(url2, form);
+
+        var body = await response.Content.ReadAsStringAsync();
+        var assessment = JsonConvert.DeserializeObject<PronunciationAssessmentResponseDTO>(body);
+        
+        // return await _speechClient.CreatePronunciationAssessmentAsync(audioFile, language, referenceText);
+        return assessment ?? new PronunciationAssessmentResponseDTO();
     }
 }
