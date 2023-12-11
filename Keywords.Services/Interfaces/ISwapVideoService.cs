b0a9d6063149f7180e139cb16b61c97cba88d446
Claudiu <75113646+143473@users.noreply.github.com>
2023-12-11 22:02:11 +0100

    connected user page to videos service, adjusted responsiveness, added interface documentation (#24)

--- Keywords.Services/Interfaces/ISwapVideoService.cs
+++ Keywords.Services/Interfaces/ISwapVideoService.cs
@@ -4,6 +4,17 @@ namespace Keywords.Services.Interfaces;
 
 public interface ISwapVideoService
 {
+    /// <summary>
+    /// Retrieve a video by its id 
+    /// </summary>
+    /// <param name="videoId">Id of the video</param>
+    /// <returns>Returns the video object</returns>
     Task<Video> GetVideoAsync(Guid videoId);
-    Task<PaginatedVideosResponse> GetVideoAsync(int? page, int size);
+    /// <summary>
+    /// Get a paginated list of videos
+    /// </summary>
+    /// <param name="page">Page number to retrieve</param>
+    /// <param name="size">Size of the page</param>
+    /// <returns>Returns a paginated list of videos</returns>
+    Task<PaginatedVideosResponse> GetAllVideosAsync(int? page, int size);
 }
--- Keywords.Services/Interfaces/ITextToSpeechService.cs
+++ Keywords.Services/Interfaces/ITextToSpeechService.cs
@@ -2,5 +2,9 @@ namespace Keywords.Services.Interfaces;
 
 public interface ITextToSpeechService
 {
+    /// <summary>
+    /// Creates an audio file for the keyword and attaches its url to the keyword object
+    /// </summary>
+    /// <param name="id">Id of the keyword</param>
     Task CreateAudioForKeyword(Guid id);
 }
